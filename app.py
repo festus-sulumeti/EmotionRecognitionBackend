a71378f5e32a7468dbad77a6773263457d5f6e38
festus-sulumeti <sulumetifestus@outlook.com>
2024-09-25 17:36:12 +0300

    project files

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_cors import CORS
 from models import db, User
+from video_processing import generate_frames
 import os
 from dotenv import load_dotenv
 
